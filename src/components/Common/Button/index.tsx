0b73c0ad48bf94ffb0af932ee2f77b25a76bee56
ding-co <pangoons@naver.com>
2023-08-19 04:15:55 +0900

    feat: Add Button component

---
+++ src/components/Common/Button/index.tsx
@@ -0,0 +1,40 @@
+import { ButtonHTMLAttributes, ReactElement } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  buttonType: 'Primary' | 'Secondary';
+  size: 'lg' | 'sm';
+  text: string;
+  customClassName?: string;
+  onClick?: () => void;
+}
+
+const Button = ({
+  buttonType,
+  type,
+  size,
+  text,
+  disabled = false,
+  customClassName,
+  onClick,
+}: Props): ReactElement => {
+  const buttonStyles = {
+    backgroundColor: buttonType === 'Primary' ? 'bg-orange' : 'bg-white',
+    fontColor: buttonType === 'Primary' ? 'text-white' : 'text-orange',
+    borderRadius: size === 'lg' ? 'rounded-[8px]' : 'rounded-[6px]',
+    padding: buttonType === 'Primary' ? 'py-[13px]' : 'py-[5.76px]',
+  } as const;
+  return (
+    <button
+      type={type ?? 'button'}
+      className={`flex justify-center items-center ${Object.values(
+        buttonStyles,
+      ).join(' ')} ${customClassName ?? ''}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
