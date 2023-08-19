b273afd4a548aa006d708f11b509891a0c6301d1
ding-co <pangoons@naver.com>
2023-08-19 21:28:15 +0900

    refactor: HomePageMain - useBadModalActions

---
+++ src/hooks/HomePage/useBedModalActions.tsx
@@ -0,0 +1,208 @@
+import { queryClient } from '@/react-query/queryClient';
+import { queryKeys } from '@/react-query/queryKeys';
+import { useCallback, useEffect, useState } from 'react';
+
+import { useModal } from '@/components/Common/Modal/Modal.hooks';
+
+import { INITIAL_FORM } from '@/constants/HomePage/form';
+
+import { Bed } from '@/api/models/bed/bed.type';
+
+import { usePostBedOutMutation } from '../query/bed/useBedMutation';
+import {
+  usePostPatientAdmission,
+  usePutPatientBedInMutation,
+} from '../query/patient/usePatientMutation';
+import useBedModalChildrenForm, { ModalType } from './useBedModalChildrenForm';
+
+const useBedModalActions = () => {
+  const [selectedBed, setSelectedBed] = useState<Bed | null>(null);
+  const [modalType, setModalType] = useState<ModalType | null>(null);
+
+  const reset = () => {
+    setSelectedBed(null);
+    setModalType(null);
+    setForm(INITIAL_FORM);
+  };
+
+  const { isOpen, open, close, setData, render } = useModal({
+    onCustomCloseAction: reset,
+  });
+
+  const { bedModalChildren, form, setForm } = useBedModalChildrenForm();
+
+  const { mutate: postBedOutMutate } = usePostBedOutMutation();
+  const { mutate: postPatientMutate } = usePostPatientAdmission();
+  const { mutate: putPatientMutate } = usePutPatientBedInMutation();
+
+  const handleAddBed = useCallback(() => {
+    setModalType('Add');
+    setData({
+      modalChildren: bedModalChildren('Add'),
+      buttons: [
+        {
+          type: 'Secondary',
+          text: 'Cancel',
+          action: () => {
+            reset();
+            close();
+          },
+        },
+        {
+          type: 'Primary',
+          text: 'Save',
+          action: () => {
+            // FIXME: Wrong Hospital...?
+            postPatientMutate(
+              {
+                bedId: Number(form.bedCode),
+                patientId: form.patientCode,
+                name: form.name,
+                gender: form.gender,
+                diagnosis: form.diagnosis,
+                birthYear: Number(form.birthYear),
+                birthMonth: Number(form.birthMonth),
+                birthDay: Number(form.birthDay),
+                severity: form.severity,
+                etaHour: Number(form.etaHour),
+                etaMin: Number(form.etaMin),
+              },
+              {
+                onSuccess: () => {
+                  queryClient.invalidateQueries([queryKeys.GetBeds]);
+                },
+              },
+            );
+
+            reset();
+            close();
+          },
+        },
+      ],
+    });
+
+    open();
+  }, [bedModalChildren]);
+
+  const handleEditBed = useCallback(
+    (bed: Bed) => {
+      setSelectedBed(bed);
+      setModalType('Edit');
+      setData({
+        modalChildren: bedModalChildren('Edit'),
+        buttons: [
+          {
+            type: 'Secondary',
+            text: 'Cancel',
+            action: () => {
+              reset();
+              close();
+            },
+          },
+          {
+            type: 'Primary',
+            text: 'Save',
+            action: () => {
+              // TODO: 테스트 필요
+              if (bed.patientId && form.name)
+                putPatientMutate(
+                  {
+                    patientId: bed.patientId?.toString(),
+                    payload: {
+                      name: form.name,
+                      gender: form.gender,
+                      diagnosis: form.diagnosis,
+                      birthYear: Number(form.birthYear),
+                      birthMonth: Number(form.birthMonth),
+                      birthDay: Number(form.birthDay),
+                      severity: form.severity,
+                      etaHour: Number(form.etaHour),
+                      etaMin: Number(form.etaMin),
+                    },
+                  },
+                  {
+                    onSuccess: () => {
+                      queryClient.invalidateQueries([queryKeys.GetBeds]);
+                    },
+                  },
+                );
+              reset();
+              close();
+            },
+          },
+        ],
+      });
+
+      open();
+    },
+    [bedModalChildren],
+  );
+
+  const handleDischargeBed = useCallback(
+    (bed: Bed) => {
+      setSelectedBed(bed);
+      setModalType('Discharge');
+      setData({
+        modalChildren: bedModalChildren('Discharge'),
+        buttons: [
+          {
+            type: 'Secondary',
+            text: 'Cancel',
+            action: () => {
+              reset();
+              close();
+            },
+          },
+          {
+            type: 'Primary',
+            text: 'Discharge',
+            action: () => {
+              postBedOutMutate(bed.id.toString(), {
+                onSuccess: () => {
+                  // TODO: 테스트 필요
+                  queryClient.invalidateQueries([queryKeys.GetBeds]);
+                },
+              });
+              reset();
+              close();
+            },
+          },
+        ],
+      });
+
+      open();
+    },
+    [bedModalChildren],
+  );
+
+  useEffect(() => {
+    if (isOpen) {
+      switch (modalType) {
+        case 'Add':
+          handleAddBed();
+          break;
+        case 'Edit':
+          if (selectedBed) {
+            handleEditBed(selectedBed);
+          }
+          break;
+        case 'Discharge':
+          if (selectedBed) {
+            handleDischargeBed(selectedBed);
+          }
+          break;
+      }
+    }
+  }, [
+    isOpen,
+    modalType,
+    handleAddBed,
+    handleEditBed,
+    handleDischargeBed,
+    selectedBed,
+  ]);
+
+  return { handleAddBed, handleEditBed, handleDischargeBed, isOpen, render };
+};
+
+export default useBedModalActions;
--- src/pageComponents/HomePageMain.tsx
+++ src/pageComponents/HomePageMain.tsx
@@ -1,222 +1,30 @@
-import { queryClient } from '@/react-query/queryClient';
-import { queryKeys } from '@/react-query/queryKeys';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Button from '@/components/Common/Button';
 import Input from '@/components/Common/Input';
-import { useModal } from '@/components/Common/Modal/Modal.hooks';
 import BedCard from '@/components/Feature/BedCard';
 import HomeLayout from '@/components/Feature/Layout/HomeLayout';
 
-import { INITIAL_FORM } from '@/constants/HomePage/form';
-
 import { Bed } from '@/api/models/bed/bed.type';
 
-import useBedModalChildrenForm, {
-  ModalType,
-} from '@/hooks/HomePage/useBedModalChildrenForm';
+import useBedModalActions from '@/hooks/HomePage/useBedModalActions';
+import useBedModalChildrenForm from '@/hooks/HomePage/useBedModalChildrenForm';
 import useSearchKeyword from '@/hooks/HomePage/useSearchKeyword';
-import { usePostBedOutMutation } from '@/hooks/query/bed/useBedMutation';
 import { useGetBedsQuery } from '@/hooks/query/bed/useBedQuery';
-import {
-  usePostPatientAdmission,
-  usePutPatientBedInMutation,
-} from '@/hooks/query/patient/usePatientMutation';
 
 const HomePageMain = () => {
   const [beds, setBeds] = useState<Bed[]>([]);
-  const [selectedBed, setSelectedBed] = useState<Bed | null>(null);
-  const [modalType, setModalType] = useState<ModalType | null>(null);
 
   const { searchValue, searchKeyword, handleSearchKeyword } =
     useSearchKeyword();
 
-  const reset = () => {
-    setSelectedBed(null);
-    setModalType(null);
-    setForm(INITIAL_FORM);
-  };
-
-  const { isOpen, open, close, setData, render } = useModal({
-    onCustomCloseAction: reset,
-  });
-
-  const { bedModalChildren, form, setForm } = useBedModalChildrenForm();
+  const { setForm } = useBedModalChildrenForm();
+  const { handleAddBed, handleEditBed, handleDischargeBed, isOpen, render } =
+    useBedModalActions();
 
   const { data, isLoading } = useGetBedsQuery({
     s: searchKeyword,
   });
-
-  const { mutate: postBedOutMutate } = usePostBedOutMutation();
-  const { mutate: postPatientMutate } = usePostPatientAdmission();
-  const { mutate: putPatientMutate } = usePutPatientBedInMutation();
-
-  const handleAddBed = useCallback(() => {
-    setModalType('Add');
-    setData({
-      modalChildren: bedModalChildren('Add'),
-      buttons: [
-        {
-          type: 'Secondary',
-          text: 'Cancel',
-          action: () => {
-            reset();
-            close();
-          },
-        },
-        {
-          type: 'Primary',
-          text: 'Save',
-          action: () => {
-            // FIXME: Wrong Hospital...?
-            postPatientMutate(
-              {
-                bedId: Number(form.bedCode),
-                patientId: form.patientCode,
-                name: form.name,
-                gender: form.gender,
-                diagnosis: form.diagnosis,
-                birthYear: Number(form.birthYear),
-                birthMonth: Number(form.birthMonth),
-                birthDay: Number(form.birthDay),
-                severity: form.severity,
-                etaHour: Number(form.etaHour),
-                etaMin: Number(form.etaMin),
-              },
-              {
-                onSuccess: () => {
-                  queryClient.invalidateQueries([queryKeys.GetBeds]);
-                },
-              },
-            );
-
-            reset();
-            close();
-          },
-        },
-      ],
-    });
-
-    open();
-  }, [bedModalChildren]);
-
-  const handleEditBed = useCallback(
-    (bed: Bed) => {
-      setSelectedBed(bed);
-      setModalType('Edit');
-      setData({
-        modalChildren: bedModalChildren('Edit'),
-        buttons: [
-          {
-            type: 'Secondary',
-            text: 'Cancel',
-            action: () => {
-              reset();
-              close();
-            },
-          },
-          {
-            type: 'Primary',
-            text: 'Save',
-            action: () => {
-              // TODO: 테스트 필요
-              if (bed.patientId && form.name)
-                putPatientMutate(
-                  {
-                    patientId: bed.patientId?.toString(),
-                    payload: {
-                      name: form.name,
-                      gender: form.gender,
-                      diagnosis: form.diagnosis,
-                      birthYear: Number(form.birthYear),
-                      birthMonth: Number(form.birthMonth),
-                      birthDay: Number(form.birthDay),
-                      severity: form.severity,
-                      etaHour: Number(form.etaHour),
-                      etaMin: Number(form.etaMin),
-                    },
-                  },
-                  {
-                    onSuccess: () => {
-                      queryClient.invalidateQueries([queryKeys.GetBeds]);
-                    },
-                  },
-                );
-              reset();
-              close();
-            },
-          },
-        ],
-      });
-
-      open();
-    },
-    [bedModalChildren],
-  );
-
-  const handleDischargeBed = useCallback(
-    (bed: Bed) => {
-      setSelectedBed(bed);
-      setModalType('Discharge');
-      setData({
-        modalChildren: bedModalChildren('Discharge'),
-        buttons: [
-          {
-            type: 'Secondary',
-            text: 'Cancel',
-            action: () => {
-              reset();
-              close();
-            },
-          },
-          {
-            type: 'Primary',
-            text: 'Discharge',
-            action: () => {
-              postBedOutMutate(bed.id.toString(), {
-                onSuccess: () => {
-                  // TODO: 테스트 필요
-                  queryClient.invalidateQueries([queryKeys.GetBeds]);
-                },
-              });
-              reset();
-              close();
-            },
-          },
-        ],
-      });
-
-      open();
-    },
-    [bedModalChildren],
-  );
-
-  useEffect(() => {
-    if (isOpen) {
-      switch (modalType) {
-        case 'Add':
-          handleAddBed();
-          break;
-        case 'Edit':
-          if (selectedBed) {
-            handleEditBed(selectedBed);
-          }
-          break;
-        case 'Discharge':
-          if (selectedBed) {
-            handleDischargeBed(selectedBed);
-          }
-          break;
-      }
-    }
-  }, [
-    isOpen,
-    modalType,
-    handleAddBed,
-    handleEditBed,
-    handleDischargeBed,
-    selectedBed,
-  ]);
 
   useEffect(() => {
     if (!data || isLoading) {
