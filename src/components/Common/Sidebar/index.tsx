eed845ef25680e56bd59e38e296e070570e135e3
ding-co <pangoons@naver.com>
2023-08-19 22:06:25 +0900

    style: Sidebar position fixed && feat: Add logout logic

--- src/components/Common/Sidebar/index.tsx
+++ src/components/Common/Sidebar/index.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-
 import { ILayoutProps } from '@/interfaces/layout.type';
 
+import { COOKIE_ACCESS_TOKEN_KEY } from '@/constants/key';
+
+import { deleteCookie } from '@/utils/cookie';
+
 const Sidebar = ({ children }: ILayoutProps) => {
+  const handleLogout = () => {
+    deleteCookie(COOKIE_ACCESS_TOKEN_KEY);
+    window.location.href = '/';
+  };
+
   return (
     <aside className="flex flex-col items-center min-w-[280px] min-h-screen bg-white border-r border-solid border-gray_200">
-      <div className="w-full border-b border-solid border-gray_200 py-5 px-8">
+      <div className="fixed min-w-[280px] border-b border-solid border-gray_200 py-5 px-8">
         <img className="w-[124px] h-[32px]" src="/logo.svg" alt="로고" />
       </div>
-      <div className="w-full flex-1 border-b border-solid border-gray_200">
-        {children}
+      <div className="fixed left-0 top-[5rem] min-w-[280px]">{children}</div>
+      <div className="fixed bottom-0 min-w-[280px] py-5 px-8 border-t border-solid border-gray_200">
+        <button
+          className="font-semibold text-gray_700 text-lg self-start flex items-center gap-2"
+          onClick={handleLogout}
+        >
+          <img src="/logout.svg" alt="로그아웃" />
+          Logout
+        </button>
       </div>
-      <button className="w-fit py-5 px-8 font-semibold text-gray_700 text-lg self-start flex items-center gap-2">
-        <img src="/logout.svg" alt="로그아웃" />
-        Logout
-      </button>
     </aside>
   );
 };
